c0de4c1b9c43d466a8f2e5d2c035b9a345a22cc6
Olehgan <o.efimov36@mail.ru>
2021-11-06 21:39:32 +0300

    split profile into components

---
+++ src/component/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Post.module.css'
+
+export const Post = () => {
+    return (
+
+                <div className={s.item}>
+                    <img src={'https://pngimg.com/uploads/mr_bean/mr_bean_PNG16.png'} alt={'photo'} />
+                    post 1
+                    <div>
+                        like
+                    </div>
+                </div>
+    )
+}
--- src/component/Profile/Profile.tsx
+++ src/component/Profile/Profile.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import s from './Profile.module.css'
+import {MyPosts} from "./MyPosts/MyPosts";
+
+
 export const Profile = () => {
     return (
         <div className={s.content}>
             <div>
                 <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt={'images'}/>
-            </div>
-            <div>
-                ava + description
-            </div>
-            <div>
-                My posts
                 <div>
-                    New post
-                </div>
-                <div>
-                    <div className={s.item}>
-                        post 1
-                    </div>
-                    <div className={s.item}>
-                        post 2
-                    </div>
+                    <MyPosts/>
                 </div>
             </div>
         </div>
