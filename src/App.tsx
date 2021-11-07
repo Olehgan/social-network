b3bd171d6023b49f903b307db92c7b48b37be1e8
Olehgan <o.efimov36@mail.ru>
2021-11-07 18:37:10 +0300

    created a new components, add Route

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,32 @@ import React from 'react';
 import './App.css';
 import {Header} from "./component/Header/Header";
 import {Navbar} from "./component/Navbar/Navbar";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {Profile} from "./component/Profile/Profile";
 import {Dialogs} from "./component/Dialogs/Dialogs";
-import {News} from "./component/News/News";
+import {News} from './component/News/News';
 import {Music} from './component/Music/Music';
-import {Settings} from './component/Settings/Settings';
+import {Settings} from "./component/Settings/Settings";
 
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path={'/profile'} render={() => <Profile/>}/>
-                    <Route path={'/dialogs'} render={() => <Dialogs/>}/>
-                    <Route path={"/news/"} render={() => <News/>}/>
-                    <Route path={"/music/"} render={() => <Music/>}/>
-                    <Route path={"/settings/"} render={() => <Settings/>}/>
-                </div>
 
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Routes>
+                    <Route path={'/profile'} element={<Profile/>}/>
+                    <Route path={'/dialogs'} element={<Dialogs/>}/>
+                    <Route path={'/news/'} element={<News/>}/>
+                    <Route path={'/music/'} element={<Music/>}/>
+                    <Route path={'/settings/'} element={<Settings/>}/>
+                </Routes>
             </div>
-        </BrowserRouter>);
+
+        </div>
+
+    );
 }
 export default App;
