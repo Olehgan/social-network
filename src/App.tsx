9229ba56e82afc95b08840de5e585093e74a1b09
Olehgan <o.efimov36@mail.ru>
2021-11-07 16:51:55 +0300

    created a new components, add Route

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import './App.css';
 import {Header} from "./component/Header/Header";
 import {Navbar} from "./component/Navbar/Navbar";
+import {BrowserRouter, Route} from "react-router-dom";
 import {Profile} from "./component/Profile/Profile";
+import {Dialogs} from "./component/Dialogs/Dialogs";
+import {News} from "./component/News/News";
+import {Music} from './component/Music/Music';
+import {Settings} from './component/Settings/Settings';
+
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Profile/>
-            </div>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path={'/profile'} render={() => <Profile/>}/>
+                    <Route path={'/dialogs'} render={() => <Dialogs/>}/>
+                    <Route path={"/news/"} render={() => <News/>}/>
+                    <Route path={"/music/"} render={() => <Music/>}/>
+                    <Route path={"/settings/"} render={() => <Settings/>}/>
+                </div>
 
-        </div>
-    );
+            </div>
+        </BrowserRouter>);
 }
 export default App;
