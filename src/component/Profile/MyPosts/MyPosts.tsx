95bc3ae520262082b902d9ec17d56a6786889b4e
Olehgan <o.efimov36@mail.ru>
2021-11-07 15:16:45 +0300

    added props to the Post component

--- src/component/Profile/MyPosts/MyPosts.tsx
+++ src/component/Profile/MyPosts/MyPosts.tsx
@@ -11,7 +11,8 @@ export const MyPosts = () => {
                 <button> Add post</button>
             </div>
             <div className={s.posts}>
-            <Post/>
+            <Post  messages='Hi my friend'/>
+            <Post  messages='Hi my brother'/>
             </div>
         </div>
 
--- src/component/Profile/MyPosts/Post/Post.tsx
+++ src/component/Profile/MyPosts/Post/Post.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import s from './Post.module.css'
 
-export const Post = () => {
+type  PostPropsType = {
+    messages:string
+}
+
+export const Post = (props:PostPropsType) => {
     return (
 
                 <div className={s.item}>
                     <img src={'https://pngimg.com/uploads/mr_bean/mr_bean_PNG16.png'} alt={'photo'} />
-                    post 1
+                    {props.messages}
                     <div>
                         like
                     </div>
+
                 </div>
     )
 }
