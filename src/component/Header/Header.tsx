e62bfc599070eb992dba320c2d410af6657466e8
Olehgan <o.efimov36@mail.ru>
2021-11-09 16:47:53 +0300

    , added Navlink

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import style from './Header.module.css'
+import {NavLink} from "react-router-dom";
 export const Header = () => {
     return (
         <header className={style.header}>
-            <img src='https://i.pinimg.com/236x/71/b3/e4/71b3e4159892bb319292ab3b76900930.jpg' alt={'image'}/>
+         <NavLink to={'/profile'} ><img src='https://i.pinimg.com/236x/71/b3/e4/71b3e4159892bb319292ab3b76900930.jpg' alt={'image'}/></NavLink>
         </header>
     )
 }
