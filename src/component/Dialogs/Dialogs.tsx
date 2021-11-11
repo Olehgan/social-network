bf5f3f0476d2303cccfabd84413ce9685964ea11
Olehgan <o.efimov36@mail.ru>
2021-11-11 17:37:51 +0300

    changed component Dialog

--- src/component/Dialogs/Dialogs.tsx
+++ src/component/Dialogs/Dialogs.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import s from './Dialogs.module.css'
 
-export const Dialogs =()=>{
+export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog +' '+ s.active}>
+                    Oleg
+                </div>
+                <div className={s.dialog}>
+                    Dimas
+                </div>
+                <div className={s.dialog}>
+                    Any
+                </div>
+                <div className={s.dialog}>
+                    Ilia
+                </div>
+            </div>
+                <div className={s.messages}>
+                    <div className={s.message}>
+                        How are you?
+                    </div>
+                    <div className={s.message}>
+                        Hello!!!!
+                    </div>
+                    <div className={s.message}>
+                        Hi!!
+                    </div>
+                </div>
+
+
         </div>
     )
 }
