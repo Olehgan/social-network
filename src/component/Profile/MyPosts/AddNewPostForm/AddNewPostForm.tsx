827a41072d997420f5d8b0061432a6a92a069c6c
Olehgan <o.efimov36@mail.ru>
2022-01-01 11:05:07 +0300

    added AddNewPostForm component

---
+++ src/component/Profile/MyPosts/AddNewPostForm/AddNewPostForm.tsx
@@ -0,0 +1,35 @@
+import {useFormik} from "formik";
+import React from "react";
+
+type AddNewPostFormType = {
+    addPost: (newPostText: string) =>void
+}
+
+export const AddNewPostForm = (props: AddNewPostFormType) => {
+    const formik = useFormik({
+        initialValues: {
+            newPostText: '',
+        },
+        onSubmit: values => {
+            props.addPost(values.newPostText)
+            formik.resetForm();
+        },
+    });
+
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <div><textarea placeholder='Enter text'
+                           name='newPostText'
+                           onChange={formik.handleChange}
+                           value={formik.values.newPostText}
+            >
+            </textarea>
+            </div>
+            <div>
+                <button type={'submit'}>
+                    Add Post
+                </button>
+            </div>
+        </form>
+    )
+}
